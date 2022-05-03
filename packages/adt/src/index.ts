a31f3baae8debe7319e2e5f930b695595c270304
Eduard Kovnatsky <eduard.kovnatsky@gmail.com>
2022-05-03 17:21:44 +0300

    fix: export types in index

--- packages/adt/src/index.ts
+++ packages/adt/src/index.ts
@@ -1,3 +1,7 @@
 export * as asyncResult from './adt/async-result'
 export * as nullable from './adt/nullable'
 export * as result from './adt/result'
+
+export type { Nullable } from './adt/nullable'
+export type { Ok, Error, Result } from './adt/result'
+export type { Initial, Loading, AsyncResult } from './adt/async-result'
